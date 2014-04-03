021f85db3b02a9e2400e32aabb4c623608e47023
Marcel Stör <marcel@frightanic.com>
2014-04-03 14:18:55 +0200

    Add configuration support for CORS allow-origin pattern. Fixes #6.

--- valdr-bean-validation/src/main/java/com/github/valdr/ValidationRulesServlet.java
+++ valdr-bean-validation/src/main/java/com/github/valdr/ValidationRulesServlet.java
@@ -19,11 +19,13 @@ import java.util.List;
 public class ValidationRulesServlet extends HttpServlet {
   private static final String MODEL_PACKAGES_CONFIG_PARAM = "modelPackages";
   private static final String CUSTOM_VALIDATORS_CONFIG_PARAM = "customValidatorClassNames";
+  private static final String CORS_PATTERN_CONFIG_PARAM = "corsAllowOriginPattern";
   private final String invalidConfigurationMessageIntro = "The Servlet is not configured correctly. ";
   private final String packageConfigurationMissingMessage = "The '" + MODEL_PACKAGES_CONFIG_PARAM + "' parameter in " +
     "web.xml is missing.";
   private final Logger logger = LoggerFactory.getLogger(ValidationRulesServlet.class);
   private boolean correctlyConfigured = false;
+  private String corsAllowOriginPattern;
   private String invalidConfigurationMessage;
   private ParserConfiguration parserConfiguration;
   private ValidationConfigurationParser parser;
@@ -31,20 +33,14 @@ public class ValidationRulesServlet extends HttpServlet {
   @Override
   public void init() throws ServletException {
     super.init();
-    assertCorrectConfiguration();
+    invalidConfigurationMessage = checkConfiguration();
     if (StringUtils.isEmpty(invalidConfigurationMessage)) {
       correctlyConfigured = true;
       logger.info("The Servlet appears to be correctly configured.");
       parserConfiguration = buildParserConfiguration();
-      logger.info("Built parser configuration '{}' based on configuration in web.xml.", parserConfiguration);
+      corsAllowOriginPattern = getCorsAllowOriginPattern();
       parser = new ValidationConfigurationParser(parserConfiguration);
     }
-  }
-
-  private ParserConfiguration buildParserConfiguration() {
-    List<String> modelPackageNames = getServletConfigAsList(MODEL_PACKAGES_CONFIG_PARAM);
-    List<String> customValidatorClassNames = getServletConfigAsList(CUSTOM_VALIDATORS_CONFIG_PARAM);
-    return new ParserConfiguration(modelPackageNames, customValidatorClassNames);
   }
 
   @Override
@@ -57,15 +53,34 @@ public class ValidationRulesServlet extends HttpServlet {
     }
   }
 
-  private void assertCorrectConfiguration() {
+  private String getCorsAllowOriginPattern() {
+    String value = getServletConfig().getInitParameter(CORS_PATTERN_CONFIG_PARAM);
+    String logMsg = "Configured CORS allow-origin pattern is '{}'.";
+    if (value == null) {
+      value = StringUtils.EMPTY;
+      logMsg += " Therefore, not using CORS.";
+    }
+    logger.info(logMsg, value);
+    return value;
+  }
+
+  private ParserConfiguration buildParserConfiguration() {
+    List<String> modelPackageNames = getServletConfigAsList(MODEL_PACKAGES_CONFIG_PARAM);
+    List<String> customValidatorClassNames = getServletConfigAsList(CUSTOM_VALIDATORS_CONFIG_PARAM);
+    ParserConfiguration config = new ParserConfiguration(modelPackageNames, customValidatorClassNames);
+    logger.info("Built parser configuration '{}' based on configuration in web.xml.", config);
+    return config;
+  }
+
+  private String checkConfiguration() {
     if (getServletConfig() == null) {
       logger.error("ServletConfig is null.");
-      invalidConfigurationMessage = buildConfigurationErrorMessage(invalidConfigurationMessage);
+      return buildConfigurationErrorMessage(invalidConfigurationMessage);
     } else if (StringUtils.isEmpty(getServletConfig().getInitParameter(MODEL_PACKAGES_CONFIG_PARAM))) {
       logger.error("'{}' parameter missing in web.xml.", MODEL_PACKAGES_CONFIG_PARAM);
-      invalidConfigurationMessage = buildConfigurationErrorMessage(invalidConfigurationMessageIntro,
-        packageConfigurationMissingMessage);
+      return buildConfigurationErrorMessage(invalidConfigurationMessageIntro, packageConfigurationMissingMessage);
     }
+    return StringUtils.EMPTY;
   }
 
   private String buildConfigurationErrorMessage(String... messages) {
@@ -77,12 +92,18 @@ public class ValidationRulesServlet extends HttpServlet {
   }
 
   private void returnJson(HttpServletResponse response, String json) throws IOException {
-    response.setHeader("Access-Control-Allow-Origin", "*");
+    setCorsHeader(response);
     response.setContentType("application/json;charset=UTF-8");
     response.setContentLength(json.getBytes("utf-8").length);
     PrintWriter writer = response.getWriter();
     writer.write(json);
     writer.close();
+  }
+
+  private void setCorsHeader(HttpServletResponse response) {
+    if (StringUtils.isNotEmpty(corsAllowOriginPattern)) {
+      response.setHeader("Access-Control-Allow-Origin", corsAllowOriginPattern);
+    }
   }
 
   private List<String> getServletConfigAsList(String paramName) {
